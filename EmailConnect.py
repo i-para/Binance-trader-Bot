1950b445c55ff5e0372e01d703068a9559761977
Behrooz Nobahar <109895703+behrooznobahar@users.noreply.github.com>
2022-12-19 17:21:29 +0330

    Add files via upload

---
+++ EmailConnect.py
@@ -0,0 +1,32 @@
+# ###############################################################################
+
+# from email.mime.text import MIMEText
+# import smtplib
+
+# ###############################################################################
+
+# class Email:
+#     def __init__(self, port, smtp_server, sender_email, receiver_email, sender_email_password):
+#         # Email settings
+#         self.port = port
+#         self.smtp_server = smtp_server
+#         self.sender_email = sender_email
+#         self.receiver_email = receiver_email
+#         self.password = sender_email_password
+
+#     def send_email(self, subject, my_message):
+#         message = MIMEText(str(my_message))
+#         message['Subject'] = subject
+#         message['From'] = self.sender_email
+#         message['To'] = ', '.join(self.receiver_email)
+#         my_server = smtplib.SMTP_SSL(self.smtp_server, self.port)
+#         my_server.login(self.sender_email, self.password)
+#         my_server.sendmail(self.sender_email,
+#                            self.receiver_email, message.as_string())
+#         my_server.quit()
+
+# ###############################################################################
+
+# # Email Accounts:
+# EMAIL_ACCOUNT = Email(port=465, smtp_server='smtp.gmail.com', sender_email='mytraderbot2022@gmail.com', receiver_email=[
+#                             'nobahar.behrooz@gmail.com', 'majidedalatjou@gmail.com'], sender_email_password='opiiikwrxmrvkrkh')
